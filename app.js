4e6ff2fdc020a997c3ab3ffc2740b02b40ac76a2
Sul-eman <56522542+Sul-eman@users.noreply.github.com>
2020-06-20 16:48:53 -0500

    added code to setup firebase with node

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+const express = require('express')
+const app = express()
+const admin = require('firebase-admin')
+const serviceAccount = require('./ServiceAccountKey.json')
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount)
+})
 
+const db = admin.firestore()
