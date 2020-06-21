85b37ade0fb70dfff2eaa6602d2583d9da3ecffe
Sul-eman <56522542+Sul-eman@users.noreply.github.com>
2020-06-20 22:46:18 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+
+app.get('/', (req, res)=>{
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(PORT, ()=>{
+  console.log(`Listening on ${PORT}`);
+});
