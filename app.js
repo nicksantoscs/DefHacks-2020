b888b78813454c3f53cc27c87518043685e5da18
Sul-eman <56522542+Sul-eman@users.noreply.github.com>
2020-06-20 20:09:19 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-const express = require('express')
-const app = express()
-const admin = require('firebase-admin')
-const serviceAccount = require('./ServiceAccountKey.json')
-admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount)
-})
+const express = require('express');
+const app = express();
+const path = require('path');
 
-const db = admin.firestore()
+/*Configure Express*/
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+/*Routes*/
+// app.use('/', (req, res) => {
+//     /*Test*/
+//     res.send("Welcome to the CMS");
+// });
+
+app.get('/', (req, res)=>{
+	res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+// PORT
+const port = process.env.POST;
+app.listen(port, () => {
+    console.log('Running on port ' + port);
+});
